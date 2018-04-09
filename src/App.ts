bbe699a37e92cbcd7f6d4ec08e80575294ea2699
Russell Blickhan <rwblickhan@gmail.com>
2018-04-09 15:35:13 -0700

    Switch to socket.io

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,4 @@
 import * as debug from "debug";
-import * as websocket from "ws";
 
 const logDebug = debug("gameio::debug");
 const logTrace = debug("gameio::trace");
@@ -7,13 +6,6 @@ const logTrace = debug("gameio::trace");
 export class App {
     public init(port: number) {
         logTrace("init()");
-        const server = new websocket.Server({port});
-        server.on("connection", (socket, _) => {
-            logDebug("New connection");
-            socket.on("message", (msg) => {
-                logDebug("New message: " + msg);
-            });
-        });
     }
 }
 
---
+++ src/Session.ts
@@ -0,0 +1,6 @@
+import * as websockets from "socket.io";
+
+// represents a single session of the game
+class Session {
+
+}
