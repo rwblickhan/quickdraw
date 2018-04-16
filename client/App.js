9bac477a1fca67ca8ad333fba535e27f5ca98c8d
Russell Blickhan <rwblickhan@gmail.com>
2018-04-15 21:38:39 -0700

    Add socket.io to client side

--- client/App.js
+++ client/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 
 class App extends Component {
-    // TODO
+    constructor(props) {
+        super(props);
+        // TODO
+        console.log("Attempting to create socket connection...");
+        this.socket = io();
+    }
 }
 
 export default App;
