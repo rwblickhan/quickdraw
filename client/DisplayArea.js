8dd6c6e3d865235b0ca325e12f94d8c4fd322c8c
Russell Blickhan <rwblickhan@gmail.com>
2018-04-16 23:40:20 -0700

    Stub out drawing and display area components

---
+++ client/DisplayArea.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class DisplayArea extends Component {
+    // represents area displaying another player's drawing
+    constructor(props) {
+        super(props);
+        // TODO
+    }
+}
---
+++ client/DrawingArea.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class DrawingArea extends Component {
+    // represents the area the player will draw into
+    constructor(props) {
+        super(props);
+        // TODO
+    }
+}
